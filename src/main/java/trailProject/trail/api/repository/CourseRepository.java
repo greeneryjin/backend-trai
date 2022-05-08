d20b400559cdb9667fa9a9d6e2aad6d803bbc771
donghwy98 <donghwy98@naver.com>
2022-05-08 13:37:33 +0900

    코스리스트, 코스디테일 조회 기능

--- src/main/java/trailProject/trail/api/repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package trailProject.trail.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import trailProject.trail.api.entity.Course;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-}
