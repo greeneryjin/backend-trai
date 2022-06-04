20e67d793f67fe93a823f2f6555f75c2e9bd2b34
donghwy98 <donghwy98@naver.com>
2022-06-04 21:20:16 +0900

    좌표 Map에서 Array로 변경

--- src/main/java/trailProject/trail/course/dto/DetailDto.java
+++ src/main/java/trailProject/trail/course/dto/DetailDto.java
@@ -3,16 +3,18 @@ package trailProject.trail.course.dto;
 import lombok.Data;
 import trailProject.trail.facility.dto.FacilityDto;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @Data
 public class DetailDto {
     private Long courseDetailId;
-    private Map<String, String> coordinateArray;
+    //private Map<String, String> coordinateArray;
+    private ArrayList<CoordinateDto> coordinateArray;
     private List<FacilityDto> facilityDtoList;
 
-    public DetailDto(Long courseDetailId, Map<String, String> coordinateArray) {
+    public DetailDto(Long courseDetailId, ArrayList<CoordinateDto> coordinateArray) { //Map<String, String> coordinateArray) {
         this.courseDetailId = courseDetailId;
         this.coordinateArray = coordinateArray;
     }
