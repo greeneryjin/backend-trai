e10103f80b72b7d71a26c0fb2a08f27b13c7c27b
김어진 <qlql12@kyonggi.ac.kr>
2022-06-11 09:56:55 +0900

    끝

--- src/main/java/trailProject/trail/history/dto/view/HistoryViewDto.java
+++ src/main/java/trailProject/trail/history/dto/view/HistoryViewDto.java
@@ -5,21 +5,28 @@ import lombok.Setter;
 import trailProject.trail.history.entity.History;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @Setter
 public class HistoryViewDto {
     private String courseName;
-    private LocalDateTime workStartTime;
+    private String workStartTime;
     private Integer distance;
     private Boolean workComplete;
     private String CourseAddress;
 
     public HistoryViewDto(History history) {
-        this.workStartTime = history.getWorkStartTime();
+        this.workStartTime = changeTimeFormat(history.getWorkStartTime());
         this.distance = history.getDistance();
         //this.workComplete = history.getWorkComplete();
         this.courseName = history.getCourseName();
         this.CourseAddress = history.getCourseAddress();
+    }
+
+    public String changeTimeFormat(LocalDateTime localDateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분");
+        String formatted = localDateTime.format(formatter);
+        return formatted;
     }
 }
